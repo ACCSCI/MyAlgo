b6e846c87cf6e79f66e63d7e1ad8ea0dcfae1c66
Marco Taylor <nonesimple@yandex.com>
2025-04-28 15:51:32 +0800

    NumOfReverseOrder

--- NumOfReverseOrder/main.cpp
+++ NumOfReverseOrder/main.cpp
@@ -1,26 +1,58 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
+
+void merge(vector<long long> &vec,long long left,long long mid,long long right,long long &count){
+    vector<long long> tmp;
+    long long i=left,j=mid+1;
+    while(i<=mid and j<=right){
+        if(vec[i]<=vec[j]){
+            tmp.push_back(vec[i]);
+            i++;
+        }else{//左大于右
+            tmp.push_back(vec[j]);
+            j++;
+            count+=mid-i+1;
+        }
+        
+    }
+    while(i<=mid){
+        tmp.push_back(vec[i]);
+        i++;
+    }
+    while(j<=right){
+        tmp.push_back(vec[j]);
+        j++;
+    }
+    for(long long k=0;k<tmp.size();k++){
+        vec[left+k]=tmp[k];
+    }
+}
+
+void mergeSort(vector<long long> &vec,long long left,long long right,long long &count){
+    if(left>=right){
+        return;
+    }
+    long long mid = (left+right)/2;
+    mergeSort(vec,left,mid,count);
+    mergeSort(vec,mid+1,right,count);
+    merge(vec,left,mid,right,count);
+}
+
+
 
 int main()
 {
-    int len;
+    long long len;
     cin>>len;
-    vector<int> vec;
-    for(int i=0;i<len;i++){
-        int n;
+    vector<long long> vec;
+    for(long long i=0;i<len;i++){
+        long long n;
         cin>>n;
         vec.push_back(n);
     }
-    int count=0;
-    for(int i=0;i<len;i++){
-        for(int j=i+1;j<len;j++){
-            if(vec[i]>vec[j]){
-                count++;
-            }
-        }
-    }
+    long long count=0;
+    mergeSort(vec,0,len-1,count);
     cout<<count;
     return 0;
 }
